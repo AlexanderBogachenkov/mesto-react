7f5c64e868640416164067b39a45b4a3f15eb3da
Alexander Bogachenkov <sashako4etov@yandex.ru>
2023-02-19 11:52:50 +0300

    10 pr start

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          10pr start 19.02.2023
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
